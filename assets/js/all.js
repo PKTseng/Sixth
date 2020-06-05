60c0561a3a7d2621b2c0a90563a4242933d5b6de
PKTseng <jk0910948659@Gmail.com>
2020-06-05 15:35:37 +0800

    sixth-hk

---
+++ assets/js/all.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  } // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+
+});
+//# sourceMappingURL=all.js.map
