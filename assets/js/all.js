e953a85279bcf30d3ad97e4ee641751d352e09cb
PKTseng <jk0910948659@Gmail.com>
2020-06-05 15:39:44 +0800

    update js

--- assets/js/all.js
+++ assets/js/all.js
@@ -13,10 +13,10 @@ var swiper = new Swiper(".swiper-container", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
-  } // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
-
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  }
 });
 //# sourceMappingURL=all.js.map
